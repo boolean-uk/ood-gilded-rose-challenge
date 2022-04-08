9ecd58887e4ed7127e83964436dd63a6948a3a95
Paulo Barata <pbara7a@gmail.com>
2022-04-08 17:55:40 +0100

    Method to update sellIn.

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -35,9 +35,8 @@ class Shop {
           }
         }
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
+      this.updateSellIn(this.items[i])
+
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
@@ -58,6 +57,10 @@ class Shop {
     }
 
     return this.items;
+  }
+
+  updateSellIn(item) {
+    if (item.name !== 'Sulfuras, Hand of Ragnaros') item.sellIn-- 
   }
 }
 module.exports = {
