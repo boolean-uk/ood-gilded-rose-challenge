4f3bb160409222bf243d98d74ab660cddb738e60
AlexDjangoX <alexmonk17@gmail.com>
2022-04-08 19:56:05 +0200

    consolidated conditionals

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -18,45 +18,39 @@ class Shop {
             const sulfurus = item.name == 'Sulfuras, Hand of Ragnaros'
 
             if (!agedBrie && !backStagePasses) {
-                if (item.quality > 0) {
-                    if (!sulfurus) {
-                        item.quality = item.quality - 1
+                if (item.quality > 0 && !sulfurus) {
+                    {
+                        item.quality--
                     }
                 }
             } else {
                 if (item.quality < 50) {
-                    item.quality = item.quality + 1
-                    if (backStagePasses) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1
+                    item.quality++
+                        if (backStagePasses) {
+                            if (item.sellIn < 11 && item.quality < 50) {
+                                item.quality++
+                            }
+                            if (item.sellIn < 6 && item.quality < 50) {
+                                item.quality++
                             }
                         }
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1
-                            }
-                        }
-                    }
                 }
             }
             if (!sulfurus) {
-                item.sellIn = item.sellIn - 1
+                item.sellIn--
             }
             if (item.sellIn < 0) {
                 if (!agedBrie) {
                     if (!backStagePasses) {
-                        if (item.quality > 0) {
-                            if (!sulfurus) {
-                                item.quality = item.quality - 1
-                            }
+                        if (item.quality > 0 && !sulfurus) {
+                            item.quality--
                         }
                     } else {
-                        item.quality = item.quality - item.quality
+                        item.quality = 0
                     }
                 } else {
                     if (item.quality < 50) {
-                        item.quality = item.quality + 1
+                        item.quality++
                     }
                 }
             }
