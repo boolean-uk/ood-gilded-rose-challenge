d1793144f531e6b6b15a02e947e809d2589d7744
AlexDjangoX <alexmonk17@gmail.com>
2022-04-11 17:38:33 +0200

    solution need to extract classes

--- spec/gildedRose.spec.js
+++ spec/gildedRose.spec.js
@@ -1,16 +1,132 @@
-var { Shop, Item } = require('../src/gilded_rose.js')
+const Shop = require('../src/gilded_rose.js')
+const Item = require('../src/item.js')
+
+describe('Aged Brie', () => {
+    it('quality increases by 1', () => {
+        const agedBrie = new Item('Aged Brie', 4, 5)
+        const items = [agedBrie]
+        const shop = new Shop(items)
+        shop.updateAgedBrie(agedBrie)
+
+        expect(agedBrie.sellIn).toEqual(3)
+        expect(agedBrie.quality).toEqual(6)
+    })
+
+    it('quality increases by 2', () => {
+        const agedBrie = new Item('Aged Brie', 0, 5)
+        const items = [agedBrie]
+        const shop = new Shop(items)
+        shop.updateAgedBrie(agedBrie)
+
+        expect(agedBrie.sellIn).toEqual(-1)
+        expect(agedBrie.quality).toEqual(7)
+    })
+
+    it('quality never more than 50', () => {
+        const agedBrie = new Item('Aged Brie', 4, 50)
+        const items = [agedBrie]
+        const shop = new Shop(items)
+        shop.updateAgedBrie(agedBrie)
+
+        expect(agedBrie.sellIn).toEqual(3)
+        expect(agedBrie.quality).toEqual(50)
+    })
+})
+
+describe('Backstage passes to a TAFKAL80ETC concert', () => {
+    it('quality increases by 1 if item quality < 50', () => {
+        const backstage = new Item(
+            'Backstage passes to a TAFKAL80ETC concert',
+            14,
+            24
+        )
+        const items = [backstage]
+        const shop = new Shop(items)
+        shop.updateBackstagePass(backstage)
+
+        expect(backstage.sellIn).toEqual(13)
+        expect(backstage.quality).toEqual(25)
+    })
+
+    it('quality increases by 2 if item.sellIn < 11 ', () => {
+        const backstage = new Item(
+            'Backstage passes to a TAFKAL80ETC concert',
+            8,
+            29
+        )
+        const items = [backstage]
+        const shop = new Shop(items)
+        shop.updateBackstagePass(backstage)
+
+        expect(backstage.sellIn).toEqual(7)
+        expect(backstage.quality).toEqual(31)
+    })
+
+    it('quality increases by 3 if item.sellIn < 6 ', () => {
+        const backstage = new Item(
+            'Backstage passes to a TAFKAL80ETC concert',
+            5,
+            35
+        )
+        const items = [backstage]
+        const shop = new Shop(items)
+        shop.updateBackstagePass(backstage)
+
+        expect(backstage.sellIn).toEqual(4)
+        expect(backstage.quality).toEqual(38)
+    })
+})
 
 describe('Gilded Rose', function() {
-    it('should return correct name', function() {
-        const gildedRose = new Shop([
-            new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20)
-        ])
+    it('Day 1: should update all items', function() {
+        const startingItems = [
+            new Item('+5 Dexterity Vest', 10, 20),
+            new Item('Aged Brie', 2, 0),
+            new Item('Elixir of the Mongoose', 5, 7),
+            new Item('Sulfuras, Hand of Ragnaros', 0, 80),
+            new Item('Sulfuras, Hand of Ragnaros', -1, 80),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49),
+
+            // This Conjured item does not work properly yet
+            new Item('Conjured Mana Cake', 3, 6)
+        ]
+
+        const gildedRose = new Shop(startingItems)
         const items = gildedRose.updateQuality()
-        expect(items[0].name).toEqual('Backstage passes to a TAFKAL80ETC concert')
+
+        // +5 Dexterity Vest
+        expect(items[0].sellIn).toEqual(9)
+        expect(items[0].quality).toEqual(19)
+            // Aged Brie
+        expect(items[1].sellIn).toEqual(1)
+        expect(items[1].quality).toEqual(1)
+            // Elixir of the Mongoose
+        expect(items[2].sellIn).toEqual(4)
+        expect(items[2].quality).toEqual(6)
+            // Sulfuras, Hand of Ragnaros
+        expect(items[3].sellIn).toEqual(0)
+        expect(items[3].quality).toEqual(80)
+            // Sulfuras, Hand of Ragnaros
+        expect(items[4].sellIn).toEqual(-1)
+        expect(items[4].quality).toEqual(80)
+            // Backstage passes to a TAFKAL80ETC concert
+        expect(items[5].sellIn).toEqual(14)
+        expect(items[5].quality).toEqual(21)
+            // Backstage passes to a TAFKAL80ETC concert
+        expect(items[6].sellIn).toEqual(9)
+        expect(items[6].quality).toEqual(50)
+            // Backstage passes to a TAFKAL80ETC concert
+        expect(items[7].sellIn).toEqual(4)
+        expect(items[7].quality).toEqual(50)
+            // Conjured Mana Cake
+        expect(items[8].sellIn).toEqual(2)
+        expect(items[8].quality).toEqual(5)
     })
 
-    it('should return array of items added to shop', function() {
-        const items = [
+    it('Day 30: should update all items', function() {
+        const startingItems = [
             new Item('+5 Dexterity Vest', 10, 20),
             new Item('Aged Brie', 2, 0),
             new Item('Elixir of the Mongoose', 5, 7),
@@ -19,159 +135,43 @@ describe('Gilded Rose', function() {
             new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
             new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49),
             new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49),
+
+            // This Conjured item does not work properly yet
             new Item('Conjured Mana Cake', 3, 6)
         ]
 
-        const expected = [
-            new Item('+5 Dexterity Vest', 9, 19),
-            new Item('Aged Brie', 1, 1),
-            new Item('Elixir of the Mongoose', 4, 6),
-            new Item('Sulfuras, Hand of Ragnaros', 0, 80),
-            new Item('Sulfuras, Hand of Ragnaros', -1, 80),
-            new Item('Backstage passes to a TAFKAL80ETC concert', 14, 21),
-            new Item('Backstage passes to a TAFKAL80ETC concert', 9, 50),
-            new Item('Backstage passes to a TAFKAL80ETC concert', 4, 50),
-            new Item('Conjured Mana Cake', 2, 5)
-        ]
-
-        const shop = new Shop(items)
-        let result = shop.updateQuality()
-
-        expect(result).toEqual(expected)
-    })
-
-    it('array of items after 10 days, quality never goes below zero', function() {
-        const items = [
-            new Item('+5 Dexterity Vest', 10, 20),
-            new Item('Aged Brie', 2, 0),
-            new Item('Elixir of the Mongoose', 5, 7),
-            new Item('Sulfuras, Hand of Ragnaros', 0, 80),
-            new Item('Sulfuras, Hand of Ragnaros', -1, 80),
-            new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
-            new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49),
-            new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49),
-            new Item('Conjured Mana Cake', 3, 6)
-        ]
-
-        const expected = [
-            new Item('+5 Dexterity Vest', 0, 10),
-            new Item('Aged Brie', -8, 18),
-            new Item('Elixir of the Mongoose', -5, 0),
-            new Item('Sulfuras, Hand of Ragnaros', 0, 80),
-            new Item('Sulfuras, Hand of Ragnaros', -1, 80),
-            new Item('Backstage passes to a TAFKAL80ETC concert', 5, 35),
-            new Item('Backstage passes to a TAFKAL80ETC concert', 0, 50),
-            new Item('Backstage passes to a TAFKAL80ETC concert', -5, 0),
-            new Item('Conjured Mana Cake', -7, 0)
-        ]
-
-        const shop = new Shop(items)
-        for (let j = 0; j < 9; j++) {
-            shop.updateQuality()
+        const gildedRose = new Shop(startingItems)
+        for (let i = 0; i < 29; i++) {
+            gildedRose.updateQuality()
         }
+        const items = gildedRose.updateQuality()
 
-        let result = shop.updateQuality()
-
-        expect(result).toEqual(expected)
-    })
-
-    it('after sell by date quality decreases at twice the rate', function() {
-        // name, sellIn, quality
-        // day 10
-        const items = [new Item('+5 Dexterity Vest', 0, 10)]
-
-        // day 11
-        const expected = [new Item('+5 Dexterity Vest', -1, 8)]
-
-        const shop = new Shop(items)
-
-        let result = shop.updateQuality()
-
-        expect(result).toEqual(expected)
-    })
-
-    it('aged brie quality goes up by one', function() {
-        // name, sellIn, quality
-        // day 10
-        const items = [new Item('Aged Brie', -7, 16)]
-
-        // day 11
-        const expected = [new Item('Aged Brie', -8, 18)]
-
-        const shop = new Shop(items)
-
-        let result = shop.updateQuality()
-
-        expect(result).toEqual(expected)
-    })
-
-    it('aged brie quality goes up by one but not more than 50', function() {
-        // name, sellIn, quality
-        // day 10
-        const items = [new Item('Aged Brie', -7, 49)]
-
-        // day 11
-        const expected = [new Item('Aged Brie', -8, 50)]
-        const expected_2 = [new Item('Aged Brie', -9, 50)]
-
-        const shop = new Shop(items)
-
-        let result = shop.updateQuality()
-        expect(result).toEqual(expected)
-        expect(result).toEqual(expected)
-
-        let result_2 = shop.updateQuality()
-
-        expect(result_2).toEqual(expected_2)
-    })
-
-    it('backstage passes quality increases by 2 when 10 days or less', function() {
-        // name, sellIn, quality
-
-        const items = [new Item('Backstage passes to a TAFKAL80ETC concert', 6, 33)]
-
-        const expected = [
-            new Item('Backstage passes to a TAFKAL80ETC concert', 5, 35)
-        ]
-
-        const shop = new Shop(items)
-
-        let result = shop.updateQuality()
-
-        expect(result).toEqual(expected)
-    })
-
-    it('backstage passes quality increases by 3 when 5 days or less', function() {
-        // name, sellIn, quality
-
-        const items = [new Item('Backstage passes to a TAFKAL80ETC concert', 5, 33)]
-
-        const expected = [
-            new Item('Backstage passes to a TAFKAL80ETC concert', 4, 36)
-        ]
-
-        const shop = new Shop(items)
-
-        let result = shop.updateQuality()
-
-        expect(result).toEqual(expected)
-    })
-
-    it('quality drops to zero after concerts on sellIn = 0', function() {
-        // name, sellIn, quality
-
-        // day 10
-        const items = [new Item('Backstage passes to a TAFKAL80ETC concert', 0, 50)]
-
-        // day 11
-        const expected = [
-            new Item('Backstage passes to a TAFKAL80ETC concert', -1, 0)
-        ]
-
-        const shop = new Shop(items)
-
-        let result = shop.updateQuality()
-
-        expect(result).toEqual(expected)
+        // +5 Dexterity Vest
+        expect(items[0].sellIn).toEqual(-20)
+        expect(items[0].quality).toEqual(0)
+            // Aged Brie
+        expect(items[1].sellIn).toEqual(-28)
+        expect(items[1].quality).toEqual(50)
+            // Elixir of the Mongoose
+        expect(items[2].sellIn).toEqual(-25)
+        expect(items[2].quality).toEqual(0)
+            // Sulfuras, Hand of Ragnaros
+        expect(items[3].sellIn).toEqual(0)
+        expect(items[3].quality).toEqual(80)
+            // Sulfuras, Hand of Ragnaros
+        expect(items[4].sellIn).toEqual(-1)
+        expect(items[4].quality).toEqual(80)
+            // Backstage passes to a TAFKAL80ETC concert
+        expect(items[5].sellIn).toEqual(-15)
+        expect(items[5].quality).toEqual(0)
+            // Backstage passes to a TAFKAL80ETC concert
+        expect(items[6].sellIn).toEqual(-20)
+        expect(items[6].quality).toEqual(0)
+            // Backstage passes to a TAFKAL80ETC concert
+        expect(items[7].sellIn).toEqual(-25)
+        expect(items[7].quality).toEqual(0)
+            // Conjured Mana Cake
+        expect(items[8].sellIn).toEqual(-27)
+        expect(items[8].quality).toEqual(0)
     })
 })
--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -1,64 +1,49 @@
-class Item {
-    constructor(name, sellIn, quality) {
-        this.name = name
-        this.sellIn = sellIn
-        this.quality = quality
-    }
-}
-
-const agedBrieMethod = (item) => {
-    item.sellIn--
-        if (item.quality < 50) item.quality++
-            if (item.sellIn < 0) item.quality = 0
-}
-
 class Shop {
     constructor(items = []) {
         this.items = items
     }
-    updateQuality() {
-        this.items.forEach((item) => {
-            const agedBrie = item.name == 'Aged Brie'
-            const backStagePasses =
-                item.name == 'Backstage passes to a TAFKAL80ETC concert'
-            const sulfurus = item.name == 'Sulfuras, Hand of Ragnaros'
 
-            if (item.name === agedBrie) {
-                this.agedBrieMethod(item)
-            } else {
-                if (!agedBrie && !backStagePasses && !sulfurus) {
-                    if (item.quality > 0) item.quality--
-                } else {
-                    if (item.quality < 50) item.quality++
-                        if (backStagePasses) {
-                            if (item.sellIn < 11 && item.quality < 50) item.quality++
-                                if (item.sellIn < 6 && item.quality < 50) item.quality++
-                        }
-                }
-                if (!sulfurus) {
-                    item.sellIn--
-                }
-                if (item.sellIn < 0) {
-                    if (!agedBrie) {
-                        if (!backStagePasses) {
-                            if (item.quality > 0 && !sulfurus) {
-                                item.quality--
-                            }
-                        } else {
-                            item.quality = 0
-                        }
-                    } else {
-                        if (item.quality < 50) {
-                            item.quality++
-                        }
-                    }
-                }
+    updateAgedBrie(item) {
+        item.sellIn--
+
+            if (item.quality < 50) {
+                item.quality = item.quality + 1
             }
-        })
+        if (item.sellIn < 0 && item.quality < 50) {
+            item.quality = item.quality + 1
+        }
+    }
+
+    updateBackstagePass(item) {
+        item.sellIn--
+            if (item.quality < 50) item.quality++
+                if (item.sellIn < 11 && item.quality < 50) item.quality++
+                    if (item.sellIn < 6 && item.quality < 50) item.quality++
+                        if (item.sellIn < 0) item.quality = 0
+    }
+
+    normalCheeses(item) {
+        item.sellIn--
+            if (item.quality < 50 && item.quality > 0) item.quality--
+                if (item.sellIn <= 0 && item.quality > 0) item.quality--
+    }
+
+    updateQuality() {
+        for (var i = 0; i < this.items.length; i++) {
+            const item = this.items[i]
+            if (item.name === 'Aged Brie') {
+                this.updateAgedBrie(item)
+            } else if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
+                this.updateBackstagePass(item)
+            } else if (item.name === 'Sulfuras, Hand of Ragnaros') {
+                continue
+            } else {
+                this.normalCheeses(item)
+            }
+        }
+
         return this.items
     }
 }
-module.exports = {
-    Item,
-    Shop
-}
+
+module.exports = Shop
