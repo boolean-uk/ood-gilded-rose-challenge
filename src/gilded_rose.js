392e5a872935741fa8a4e1d4d97fc9e5db0116f7
Marcin Jankowski <marcin.jankowski@talan.com>
2023-07-24 15:09:47 +0200

    format code

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -1,99 +1,90 @@
 class Item {
-  constructor(name, sellIn, quality){
-    this.name = name;
-    this.sellIn = sellIn;
-    this.quality = quality;
+  constructor(name, sellIn, quality) {
+    this.name = name
+    this.sellIn = sellIn
+    this.quality = quality
   }
 
-  updateQuality() {
-    
-   }
+  updateQuality() {}
 }
 
 class StandardItem extends Item {
-  constructor(name, sellIn, quality) {
-    super(name, sellIn, quality);
-  }
-
   updateQuality() {
-    this.sellIn <= 0 ? this.quality -= 2 : this.quality--;
-    this.sellIn--;
+    this.sellIn <= 0 ? (this.quality -= 2) : this.quality--
+    this.sellIn--
     if (this.quality < 0) {
-      this.quality = 0;
-    } 
+      this.quality = 0
+    }
   }
-} 
+}
 
 class AgedBrieItem extends Item {
-  constructor(name, sellIn, quality) {
-    super(name, sellIn, quality);
-  }
-
   updateQuality() {
-    this.quality++;
-    this.sellIn--;
+    this.quality++
+    this.sellIn--
     if (this.quality > 50) {
-      this.quality = 50;
-    } 
+      this.quality = 50
+    }
   }
-} 
+}
 
 class SulfurasItem extends Item {
   constructor(sellIn) {
-    super("Sulfuras, Hand of Ragnaros", sellIn, 80);
+    super('Sulfuras, Hand of Ragnaros', sellIn, 80)
   }
 
-  updateQuality() {
-  }
-} 
+  updateQuality() {}
+}
 
 class BackstagePassesItem extends Item {
   constructor(sellIn, quality) {
-    super("Backstage passes to a TAFKAL80ETC concert", sellIn, quality);
+    super('Backstage passes to a TAFKAL80ETC concert', sellIn, quality)
   }
 
   updateQuality() {
     if (this.sellIn > 10) {
-      this.quality++;
+      this.quality++
     } else if (this.sellIn > 5) {
-      this.quality += 2;
+      this.quality += 2
     } else if (this.sellIn > 0) {
-      this.quality += 3;
+      this.quality += 3
     } else {
-      this.quality = 0;
+      this.quality = 0
     }
-    this.sellIn--;
+    this.sellIn--
     if (this.quality > 50) {
-      this.quality = 50;
-    } 
+      this.quality = 50
+    }
   }
-} 
+}
 
 class ConjuredItem extends Item {
   constructor(sellIn, quality) {
-    super("Conjured Mana Cake", sellIn, quality);
+    super('Conjured Mana Cake', sellIn, quality)
   }
 
   updateQuality() {
-    this.sellIn <= 0 ? this.quality -= 4 : this.quality -= 2;
-    this.sellIn--;
+    this.sellIn <= 0 ? (this.quality -= 4) : (this.quality -= 2)
+    this.sellIn--
     if (this.quality < 0) {
-      this.quality = 0;
-    } 
-  }
-} 
-
-class Shop {
-  constructor(items=[]){
-    this.items = items;
-  }
-  updateQualityOfItems() {
-    for (var i = 0; i < this.items.length; i++) {
-      this.items[i].updateQuality();
+      this.quality = 0
     }
-    return this.items;
   }
 }
+
+class Shop {
+  constructor(items = []) {
+    this.items = items
+  }
+
+  updateQualityOfItems() {
+    for (let i = 0; i < this.items.length; i++) {
+      this.items[i].updateQuality()
+    }
+    return this.items
+  }
+}
+
 module.exports = {
   Item,
   StandardItem,
